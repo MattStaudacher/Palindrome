9d9fd262b130bdd26773c74303b15f11e6cbcf3d
Brian Palowski, Matt Staudacher and Steven Colburn <brian@>
2018-06-25 14:09:46 -0700

    testing hard version

--- palindrome.cs
+++ palindrome.cs
@@ -5,65 +5,88 @@ public class Program
 {
   public static void Main()
   {
+    //Console.WriteLine("Input");
+
+    // PalindromeEasy easyP = new PalindromeEasy();
+    // easyP.Input();
+
     PalindromeHard palindrome = new PalindromeHard();
     palindrome.GetUserInput();
     palindrome.IsPalindrome();
-
   }
 }
 
-class PalindromeHard
+class PalindromeEasy
 {
   private string _palindrome;
 
-  public PalindromeHard(string palindrome)
-  {
-    _palindrome = palindrome;
-  }
-
-  public PalindromeHard()
+   public PalindromeEasy()
   {
     _palindrome = "";
+   }
+
+  public void Input()
+  {
+    Console.WriteLine("Input");
   }
 
-  public void GetUserInput()
+}
+
+
+
+
+class PalindromeHard
+{
+ private string _palindrome;
+
+  public PalindromeHard()
+ {
+   _palindrome = "";
+  }
+
+ public PalindromeHard(string palindrome)
+ {
+   _palindrome = palindrome;
+  }
+
+ public void GetUserInput()
   {
     Console.WriteLine("Input: ");
-    _palindrome = Console.ReadLine().Trim();
-  }
+   _palindrome = Console.ReadLine().Trim();
+ }
 
-  private bool CompareEnds()
-  {
-    return (_palindrome[0] == _palindrome[_palindrome.Length-1]);
-  }
+ private bool CompareEnds()
+ {
+   return (_palindrome[0] == _palindrome[_palindrome.Length-1]);
+ }
 
-  private void RemoveEnds()
-  {
-    if(_palindrome.Length >= 2)
-    {
-      _palindrome = _palindrome.Remove(0,1);
-      _palindrome = _palindrome.Remove(_palindrome.Length-1,1);
-    }
-  }
+ private void RemoveEnds()
+ {
+   if(_palindrome.Length >= 2)
+   {
+     _palindrome = _palindrome.Remove(0,1);
+     _palindrome = _palindrome.Remove(_palindrome.Length-1,1);
+   }
+ }
 
-  private void Output()
-  {
-    Console.WriteLine(_palindrome);
-  }
+ private void Output()
+ {
+   Console.WriteLine(_palindrome);
+ }
 
-  public void IsPalindrome()
-  {
-    bool isPalindrome = true;
-    while(isPalindrome && _palindrome.Length > 1)
-    {
-      isPalindrome = CompareEnds();
-      if(isPalindrome)
-      {
-        RemoveEnds();
-      }
-      //Output();
-    }
-    Console.WriteLine("Palindrome: "+isPalindrome);
-  }
+ public void IsPalindrome()
+ {
+   bool isPalindrome = true;
+   while(isPalindrome && _palindrome.Length > 1)
+   {
+     isPalindrome = CompareEnds();
+     if(isPalindrome)
+     {
+       RemoveEnds();
+     }
+      Output();
+   }
+   Console.WriteLine("Palindrome: "+isPalindrome);
+ }
 
 }
