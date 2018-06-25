07b93a4476d7acc6c1ae992e13e6d10fe8a7133a
Brian Palowski, Matt Staudacher and Steven Colburn <brian+matt+steven@>
2018-06-25 15:45:50 -0700

    Finish palandrim

--- palindrome.cs
+++ palindrome.cs
@@ -7,12 +7,13 @@ public class Program
   {
     //Console.WriteLine("Input");
 
-    // PalindromeEasy easyP = new PalindromeEasy();
-    // easyP.Input();
+    PalindromeEasy easyP = new PalindromeEasy();
+    easyP.Input();
+    Console.WriteLine("Palindrome: "+easyP.IsPalindrome());
 
-    PalindromeHard palindrome = new PalindromeHard();
-    palindrome.GetUserInput();
-    palindrome.IsPalindrome();
+    // PalindromeHard palindrome = new PalindromeHard();
+    // palindrome.GetUserInput();
+    // palindrome.IsPalindrome();
   }
 }
 
@@ -20,16 +21,26 @@ class PalindromeEasy
 {
   private string _palindrome;
 
-   public PalindromeEasy()
+  public PalindromeEasy()
   {
     _palindrome = "";
-   }
+  }
 
   public void Input()
   {
-    Console.WriteLine("Input");
+    Console.WriteLine("Input: ");
+    _palindrome = Console.ReadLine().Trim();
   }
 
+  public bool IsPalindrome()
+  {
+    char[] pArray = _palindrome.ToCharArray();
+    char[] pReverse = _palindrome.ToCharArray();
+    Array.Reverse(pReverse);
+    string s1 = new string(pArray);
+    string s2 = new string(pReverse);
+    return (s1 == s2);
+  }
 }
 
 
@@ -37,56 +48,56 @@ class PalindromeEasy
 
 class PalindromeHard
 {
- private string _palindrome;
+  private string _palindrome;
 
   public PalindromeHard()
- {
-   _palindrome = "";
+  {
+    _palindrome = "";
   }
 
- public PalindromeHard(string palindrome)
- {
-   _palindrome = palindrome;
+  public PalindromeHard(string palindrome)
+  {
+    _palindrome = palindrome;
   }
 
- public void GetUserInput()
+  public void GetUserInput()
   {
     Console.WriteLine("Input: ");
-   _palindrome = Console.ReadLine().Trim();
- }
+    _palindrome = Console.ReadLine().Trim();
+  }
 
- private bool CompareEnds()
- {
-   return (_palindrome[0] == _palindrome[_palindrome.Length-1]);
- }
+  private bool CompareEnds()
+  {
+    return (_palindrome[0] == _palindrome[_palindrome.Length-1]);
+  }
 
- private void RemoveEnds()
- {
-   if(_palindrome.Length >= 2)
-   {
-     _palindrome = _palindrome.Remove(0,1);
-     _palindrome = _palindrome.Remove(_palindrome.Length-1,1);
-   }
- }
+  private void RemoveEnds()
+  {
+    if(_palindrome.Length >= 2)
+    {
+      _palindrome = _palindrome.Remove(0,1);
+      _palindrome = _palindrome.Remove(_palindrome.Length-1,1);
+    }
+  }
 
- private void Output()
- {
-   Console.WriteLine(_palindrome);
- }
+  private void Output()
+  {
+    Console.WriteLine(_palindrome);
+  }
 
- public void IsPalindrome()
- {
-   bool isPalindrome = true;
-   while(isPalindrome && _palindrome.Length > 1)
-   {
-     isPalindrome = CompareEnds();
-     if(isPalindrome)
-     {
-       RemoveEnds();
-     }
+  public void IsPalindrome()
+  {
+    bool isPalindrome = true;
+    while(isPalindrome && _palindrome.Length > 1)
+    {
+      isPalindrome = CompareEnds();
+      if(isPalindrome)
+      {
+        RemoveEnds();
+      }
       Output();
-   }
-   Console.WriteLine("Palindrome: "+isPalindrome);
- }
+    }
+    Console.WriteLine("Palindrome: "+isPalindrome);
+  }
 
 }
